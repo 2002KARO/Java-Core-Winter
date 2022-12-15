7216c9a106562b15a67643da14303ec2171791f3
2002KARO <iskanyankaro8@gmail.com>
2022-12-15 21:02:58 +0400

    HOMEWORK

---
+++ src/chapter2/BooleanTest.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        boolean y=true;
+        boolean u=false;
+        System.out.println(y);
+        System.out.println(u);
+        
+    }
+}
