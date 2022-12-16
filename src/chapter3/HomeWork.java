e209d471f1b9f57ff8df385c07d1e53407a34622
2002KARO <iskanyankaro8@gmail.com>
2022-12-16 17:03:20 +0400

    HOMEWORK

---
+++ src/chapter3/HomeWork.java
@@ -0,0 +1,36 @@
+package chapter3;
+
+public class HomeWork {
+    /*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    * * * * * *
+    * * * * * * *
+    * * * * * * * *
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j <=i ; j++) {
+                System.out.print("* ");}
+
+                System.out.println();
+
+        }
+    }
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
