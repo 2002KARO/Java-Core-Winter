eb6bc321a3b651e2c5a254a555f139ad8eb90a47
2002KARO <iskanyankaro8@gmail.com>
2022-12-19 16:29:33 +0400

    HOMEWORK

---
+++ src/homework1/FiguarePainter2.java
@@ -0,0 +1,29 @@
+package homework1;
+
+public class FiguarePainter2 {
+
+    /*
+         *
+       * *
+     * * *
+   * * * *
+ * * * * *
+
+     */
+    public static void main(String[] args) {
+        int p=5;
+
+        for (int i = 0; i <=p ; i++) {
+            for (int j = i; j < p ; j++) {
+                System.out.print(" ");}
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");}
+            System.out.println( );
+
+        }
+
+
+
+        }
+
+    }
