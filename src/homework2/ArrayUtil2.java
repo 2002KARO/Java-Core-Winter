7cf3e143d52acaa02037116293f48223c7286fbc
2002KARO <iskanyankaro8@gmail.com>
2022-12-26 17:20:50 +0400

    HOMEWORK

--- src/homework2/ArrayUtil2.java
+++ src/homework2/ArrayUtil2.java
@@ -2,19 +2,19 @@ package homework2;
 
 public class ArrayUtil2 {
     public static void main(String[] args) {
-        int[]myShorts={12,33,6,90,88,7,5,0};
-        int arrlen = myShorts.length;
+        int[]Shortsarr={12,33,6,90,88,7,5,0};
+        int arrlen = Shortsarr.length;
         int numeralsCount = 0;
         int notnumeralsCount = 0;
         int arrsum = 0;
-        int arrFirstElement=myShorts[0];
-        int arrLastElement=myShorts[arrlen -1];
-        int arrMiddleElement = myShorts[arrlen /2];
+        int arrFirstElement=Shortsarr[0];
+        int arrLastElement=Shortsarr[arrlen -1];
+        int arrMiddleElement = Shortsarr[arrlen /2];
         //all elements
         System.out.println("All Elemenets; ");
-        for (int i = 0; i < myShorts.length ; i++) {
-            arrsum+=myShorts[i];
-            System.out.print(i+")" +myShorts[i]);
+        for (int i = 0; i < Shortsarr.length ; i++) {
+            arrsum+=Shortsarr[i];
+            System.out.print(i+")" +Shortsarr[i]);
             if (i!=arrlen -1);{
                 System.out.print(", ");
             }
@@ -22,10 +22,10 @@ public class ArrayUtil2 {
             //All numerals not numerals
             // numerals and not numerals count
             System.out.println();
-            for (int j = 0; j < myShorts.length; j++) {
-                if (myShorts[i]% 2 == 0){
+            for (int j = 0; j < Shortsarr.length; j++) {
+                if (Shortsarr[i]% 2 == 0){
                     numeralsCount++;{
-                        System.out.print(myShorts[i]);}
+                        System.out.print(Shortsarr[i]);}
                     if (i != arrlen -1){
                         System.out.print(", ");
                     }notnumeralsCount ++;
