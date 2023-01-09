fb51196f6a71acb0c2ddf88007e03d0edd5ba67a
2002KARO <iskanyankaro8@gmail.com>
2023-01-09 19:42:53 +0400

    HOMEWORK

---
+++ src/chapter3/homework5/ArraySpaceExample.java
@@ -0,0 +1,32 @@
+package chapter3.homework5;
+
+public class ArraySpaceExample {
+    public static void main(String[] args) {
+        char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
+        int started =0;
+        int finalid= spaceArray.length;
+        for (char value : spaceArray) {
+            if (value!= ' '){
+                break;
+            }started++;
+
+        }
+        for (int i = spaceArray.length-1; i > 0; i--) {
+            if (spaceArray[i]!= ' '){
+                break;
+            }
+            finalid--;
+        }
+        char[]result=new char[finalid-(started-1)];
+        int a=0;
+        for (int i = started; i <finalid+1 ; i++) {
+            result[a]=spaceArray[i];
+            a++;
+
+        }
+        for (Character c : result) {
+            System.out.println(c);
+
+        }
+    }
+}
