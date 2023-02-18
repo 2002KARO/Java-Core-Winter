9bd6237e3030f3ae656e20a081f5c60eead969d2
2002KARO <iskanyankaro8@gmail.com>
2023-02-18 13:50:18 +0400

    BookExample

--- src/group2/classwork/book/Book.java
+++ src/group2/classwork/book/Book.java
@@ -2,10 +2,50 @@ package group2.classwork.book;
 
 public class Book {
 
-    String title;
-    String author;
-    double price;
+    private String title;
+    private String author;
+    private double price;
 
+    public Book(String title,String author,double price) {
+        this.title = title;
+        this.author=author;
+        this.price=price;
 
+    }
+    public Book(){
 
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
